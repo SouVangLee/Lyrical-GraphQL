7992489df00d1bde70627157bb97b3c51b431d47
SouVangLee <sou_lee92@yahoo.com>
2022-08-08 13:22:34 -0700

    Update dependencies

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const models = require('./models');
-const expressGraphQL = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const schema = require('./schema/schema');
+const { MONGO_URI } = process.env;
 
 const app = express();
 
-// Replace with your mongoLab URI
-const MONGO_URI = '';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
@@ -20,7 +20,7 @@ mongoose.connection
     .on('error', error => console.log('Error connecting to MongoLab:', error));
 
 app.use(bodyParser.json());
-app.use('/graphql', expressGraphQL({
+app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
 }));
