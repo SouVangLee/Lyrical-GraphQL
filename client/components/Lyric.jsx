feb36ee85b82861b008b0a7a5ef70127a9423a52
SouVangLee <sou_lee92@yahoo.com>
2022-08-15 20:24:26 -0700

    Create Lyric component to refactor LyricList

---
+++ client/components/Lyric.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThumbsUp as solidThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsUp as regularThumbsUp } from '@fortawesome/free-regular-svg-icons';
+import LIKE_LYRIC from '../mutations/likeLyric';
+
+
+const Lyric = ({id, content, likes}) => {
+    const [showSolid, setShowSolid] = useState(false);
+    const [likeLyric] = useMutation(LIKE_LYRIC);
+
+    const onLike = () => {
+      if (showSolid) return;
+      if (!showSolid) {
+        setShowSolid(true);
+        /* 
+          likeLyric is the name of the actual mutation inside of gql:
+          likeLyric(id: $id) {
+            id
+            likes
+          }
+        */
+        likeLyric({ 
+          variables: { id },
+          optimisticResponse: {
+            likeLyric: {
+              id,
+              likes: likes + 1,
+              __typename: "LyricType"
+            }
+          }
+        });
+      };
+    };
+
+    return (
+      <div key={id} className="lyric-container">
+        <div>{ content }</div>
+        <div className="likes-container">
+          <div onClick={onLike} className="like-button">
+            { !showSolid ? (
+              <FontAwesomeIcon icon={regularThumbsUp} />
+            ) : (
+              <FontAwesomeIcon icon={solidThumbsUp} />
+            )}
+          </div>
+          <div>{likes}</div>
+        </div>
+      </div>
+    );
+  };
+
+export default Lyric;
--- client/components/LyricList.jsx
+++ client/components/LyricList.jsx
@@ -1,53 +1,10 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faThumbsUp as solidThumbsUp } from '@fortawesome/free-solid-svg-icons';
-import { faThumbsUp as regularThumbsUp } from '@fortawesome/free-regular-svg-icons';
-import LIKE_LYRIC from '../mutations/likeLyric';
+import React from 'react';
+import Lyric from './Lyric';
 
 const LyricList = (props) => {
-  const renderLyrics = props.lyrics.map(({ id, content, likes }) => {
-    const [showSolid, setShowSolid] = useState(false);
-    const [likeLyric] = useMutation(LIKE_LYRIC);
-
-    const onLike = () => {
-      if (showSolid) return;
-      if (!showSolid) {
-        setShowSolid(true);
-        /* 
-          likeLyric is the name of the actual mutation inside of gql:
-          likeLyric(id: $id) {
-            id
-            likes
-          }
-        */
-        likeLyric({ 
-          variables: { id },
-          optimisticResponse: {
-            likeLyric: {
-              id,
-              likes: likes + 1,
-              __typename: "LyricType"
-            }
-          }
-        });
-      };
-    };
-
+  const renderLyrics = props.lyrics.map(({ id, content, likes}) => {
     return (
-      <div key={id} className="lyric-container">
-        <div>{ content }</div>
-        <div className="likes-container">
-          <div onClick={onLike} className="like-button">
-            { !showSolid ? (
-              <FontAwesomeIcon icon={regularThumbsUp} />
-            ) : (
-              <FontAwesomeIcon icon={solidThumbsUp} />
-            )}
-          </div>
-          <div>{likes}</div>
-        </div>
-      </div>
+      <Lyric key={id} id={id} content={content} likes={likes} />
     );
   });
 
